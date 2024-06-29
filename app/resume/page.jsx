82e60f1bbfe117ed7227e5920b2845c78b35a8a5
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 11:24:16 -0400

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -157,7 +157,7 @@ const Resume = () => {
         opacity: 1, 
         transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
 
-      className="min-h-[80vh] flex items-center justify-cente py-12 xl:py-8"
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-8"
     >
       <div className="container mx-auto">
         <Tabs 
