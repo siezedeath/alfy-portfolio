489b587776709ddcca08322e295b0e69a0749671
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 11:21:07 -0400

    fixing the resume tab
    changes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -139,12 +139,48 @@ const skills = {
 }
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { 
+  Tooltip, 
+  TooltipContent, 
+  TooltipProvider, 
+  TooltipTrigger 
+} from "@radix-ui/react-tooltip";
 
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { motion } from "framer-motion";
 
-const resume = () => {
+const Resume = () => {
   return (
-    <div>resume page</div>
-  )
+    <motion.div 
+      initial={{ opacity: 0 }} 
+      animate={{ 
+        opacity: 1, 
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
+
+      className="min-h-[80vh] flex items-center justify-cente py-12 xl:py-8"
+    >
+      <div className="container mx-auto">
+        <Tabs 
+        defaultValue="experience" 
+        className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experince">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+          {/** TabsContent */}
+          <div className="min-h-[70vh] w-full">
+            {/** Experience */}
+            <TabsContent value="experience" className="w-full">
+              experience
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
-export default resume
+export default Resume
