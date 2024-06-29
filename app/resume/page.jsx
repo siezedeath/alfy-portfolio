16d705aec703a3352db413d77853ca051fe7c540
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 14:47:55 -0400

    updating each of the subcategs for resume page
    :)

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -92,7 +92,7 @@ const education = {
     {
       institution: "Stuyvesant High School",
       degree: "Applied Mathematics and Statistics",
-      duration: "August 2021 - May 2025"
+      duration: "September 2017 - June 2021"
     },
   ],
 };
@@ -170,7 +170,7 @@ const Resume = () => {
             <TabsTrigger value="skills">Skills</TabsTrigger>
             <TabsTrigger value="about">About Me</TabsTrigger>
           </TabsList>
-          {/** TabsContent */}
+          {/** Content */}
           <div className="min-h-[70vh] w-full">
             {/** Experience */}
             <TabsContent value="experience" className="w-full">
@@ -180,13 +180,13 @@ const Resume = () => {
                 <ScrollArea className="h-[400px]">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {experience.items.map((item, index) => {
-                      return <li key={index}>
-                        <span>{item.duration}</span>
-                        <h3>{item.position}</h3>
-                        <div>
+                      return <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                        <div className="flex items-center gap-3">
                           {/* dot */}
-                          <span></span>
-                          <p>{item.company}</p>
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
                         </div>
                       </li>
                     })}
@@ -195,12 +195,27 @@ const Resume = () => {
               </div>
             </TabsContent>
 
-
-
-
             {/** Education */}
             <TabsContent value="education" className="w-full">
-              education
+            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.decripton}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                        <div className="flex items-center gap-3">
+                          {/* dot */}
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.institution}</p>
+                        </div>
+                      </li>
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/** Experience */}
             <TabsContent value="skills" className="w-full">
