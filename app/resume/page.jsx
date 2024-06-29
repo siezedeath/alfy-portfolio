5da82959a7757dfb074eedd003d80f9f33239cde
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 15:47:50 -0400

    changed stuff n fixed up the education + skills tab
    changed a bit of the education const var bc the formatting was being weird, now fixed

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -86,12 +86,12 @@ const education = {
   items: [
     {
       institution: "Stony Brook University",
-      degree: "B.S. in Applied Mathematics and Statistics",
+      degree: "Applied Mathematics & Statistics",
       duration: "August 2021 - May 2025"
     },
     {
       institution: "Stuyvesant High School",
-      degree: "Applied Mathematics and Statistics",
+      degree: "Competitive Math & Computer Science",
       duration: "September 2017 - June 2021"
     },
   ],
@@ -217,11 +217,38 @@ const Resume = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
-            {/** Experience */}
-            <TabsContent value="skills" className="w-full">
-              skills
+
+
+            {/** skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return <li key={index}>
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                            <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                              {skill.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  })}
+                </ul>
+              </div>
             </TabsContent>
-            {/** Experience */}
+
+
+            {/** about */}
             <TabsContent value="about" className="w-full">
               about
             </TabsContent>
