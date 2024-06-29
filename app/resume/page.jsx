626ed135b22db0366204a8864badaa3db909f11f
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 14:20:00 -0400

    elaboration on resume
    elaborated on resume page and changed ui of tab.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -165,7 +165,7 @@ const Resume = () => {
         className="flex flex-col xl:flex-row gap-[60px]"
         >
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
-            <TabsTrigger value="experince">Experience</TabsTrigger>
+            <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
             <TabsTrigger value="about">About Me</TabsTrigger>
@@ -174,7 +174,41 @@ const Resume = () => {
           <div className="min-h-[70vh] w-full">
             {/** Experience */}
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.decripton}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return <li key={index}>
+                        <span>{item.duration}</span>
+                        <h3>{item.position}</h3>
+                        <div>
+                          {/* dot */}
+                          <span></span>
+                          <p>{item.company}</p>
+                        </div>
+                      </li>
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+
+
+
+            {/** Education */}
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            {/** Experience */}
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            {/** Experience */}
+            <TabsContent value="about" className="w-full">
+              about
             </TabsContent>
           </div>
         </Tabs>
