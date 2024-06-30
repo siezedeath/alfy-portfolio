b75bc5709980517b57918f790e10fec5f2fb37ec
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-30 17:06:10 -0400

    bug
    Fiexed a bug will come back later

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,8 +49,8 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl-h[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#2727c] rounded-xl">
+          <div className="xl:h[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas obcaecati minus minima optio reprehenderit. Aspernatur labore, rerum consectetur expedita corrupti impedit nisi, aut vel vitae magni, aliquid pariatur voluptatum incidunt.</p>
               {/** input */}
--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     (<input
       type={type}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-white/10 bg-primary text-white shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
+        "flex h-[48px] rounded-md border border-white/10 focus:border-accent font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none",
         className
       )}
       ref={ref}
