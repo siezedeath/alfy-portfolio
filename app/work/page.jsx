d78e99f95ff004efdcea1246dd50503d66a6311a
siezedeath <135669824+siezedeath@users.noreply.github.com>
2024-06-29 20:10:57 -0400

    update
    downloaded new package + finished the /work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,113 @@
-const work = () => {
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+
+const projects = [
+  {
+    num: "01",
+    category: "Web Development",
+    title: "project 1",
+    description: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nullam vel eros sit amet arcu vestibulum accumsan in in leo.",
+    stack: [
+      { name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" },],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Web Development",
+    title: "project 2",
+    description: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nullam vel eros sit amet arcu vestibulum accumsan in in leo.",
+    stack: [
+      { name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }, 
+      { name: "React" }, { name: "Next.js" }, { name: "Framer Motion" }, { name: "Tailwind CSS" }, { name: "Vercel" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Web Development",
+    title: "project 3",
+    description: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nibh. Etiam non elit dui. Nullam vel eros sit amet arcu vestibulum accumsan in in leo.",
+    stack: [
+      { name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }, 
+      { name: "React" }, { name: "Next.js" }, { name: "Framer Motion" }, { name: "Tailwind CSS" }, { name: "Vercel" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+]
+
+const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
   return (
-    <div> work page</div>
-  )
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              {/** project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 captialize">{project.category} project</h2>
+              {/** project description */}
+              <p className="text-white/60"></p>
+              {/** stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (<li key={index} className="text-xl text-accent">
+                    {item.name}
+                    {/** remove the last comma */}
+                    {index !== project.stack.length - 1 && ", "}
+                    </li>);
+                })}
+              </ul>
+              {/** border */}
+              <div className="border border-white/20"></div>
+              {/** buttons */}
+              <div>
+                <Link>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <BsArrowUpRight />
+                      </TooltipTrigger>
+                      <TooltipContent></TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
-export default work
+export default Work;
